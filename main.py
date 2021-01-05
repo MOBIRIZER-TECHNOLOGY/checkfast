07689efabbbcd6ef2ac15baa187172a3628cd872
MOBIRIZER-TECHNOLOGY <56860980+MOBIRIZER-TECHNOLOGY@users.noreply.github.com>
2021-01-05 16:05:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,39 +11,39 @@ app = FastAPI(debug=True)
 
 
 
-@lru_cache()
-def app_setting():
-    return appinfo.setting()
+# @lru_cache()
+# def app_setting():
+#     return appinfo.setting()
 
-@app.get("/app/info", tags =["App"])
-async def app_info(setting: appinfo.setting = Depends(app_setting)):
-    return {
-        "app_name" : setting.app_name,
-        "app_version" : setting.app_version,
-        "app_framework" : setting.app_framework,
-        "app_date" : setting.app_date,
-    }
+# @app.get("/app/info", tags =["App"])
+# async def app_info(setting: appinfo.setting = Depends(app_setting)):
+#     return {
+#         "app_name" : setting.app_name,
+#         "app_version" : setting.app_version,
+#         "app_framework" : setting.app_framework,
+#         "app_date" : setting.app_date,
+#     }
 
 
-@app.middleware("http")
-async def add_process_time_header(request : Request, call_next):
-    start_time = time.time()
-    response = await call_next(request)
-    process_time = time.time() - start_time
+# @app.middleware("http")
+# async def add_process_time_header(request : Request, call_next):
+#     start_time = time.time()
+#     response = await call_next(request)
+#     process_time = time.time() - start_time
     
-    response.headers['X-Process-Time'] = str(process_time)
+#     response.headers['X-Process-Time'] = str(process_time)
 
-    return response
-
-
-@app.on_event("startup")
-async def startup():
-    await database.connect()
+#     return response
 
 
-@app.on_event("shutdown")
-async def shutdown():
-    await database.disconnect()
+# @app.on_event("startup")
+# async def startup():
+#     await database.connect()
+
+
+# @app.on_event("shutdown")
+# async def shutdown():
+#     await database.disconnect()
 
 from auth import controller as authController
 app.include_router(authController.router, tags =["Auth"])
